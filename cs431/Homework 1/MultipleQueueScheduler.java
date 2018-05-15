9fc5b873f0d281ba8c5f13e8a78bbda374e8024e
Taylor Thurlow <taylorthurlow8@gmail.com>
2018-05-15 13:27:18 -0700

    CS431: Fix multiple queue scheduler

--- cs431/Homework 1/MultipleQueueScheduler.java
+++ cs431/Homework 1/MultipleQueueScheduler.java
@@ -17,21 +17,11 @@ public class MultipleQueueScheduler extends Scheduler
     // This function does the scheduling work!
     public void tick()
     {
-        Job job, best;
-        int curPriority, bestPriority;
+        Job job;
 
         // If there were any jobs that used to be blocked, but no longer are,
-        // move them from the unblockedQueue to the end of the readyQueue, unless its
-        // estimated remaining time is less than the time of the job in the cpu.
+        // move them from the unblockedQueue to the end of the readyQueue.
         for (job = cpu.unblockedQueue.first(); job != null; job = cpu.unblockedQueue.first())
-        {
-            job.enqueueEnd(cpu.readyQueue);
-        }
-
-        // If there are any new jobs being added to the processor, move them
-        // from the newJobQueue to the end of the readyQueue, unless the remaining
-        // time is less than the current job
-        for (job = cpu.newJobQueue.first(); job != null; job = cpu.newJobQueue.first())
         {
             job.enqueueEnd(cpu.readyQueue);
         }
@@ -60,48 +50,54 @@ public class MultipleQueueScheduler extends Scheduler
             }
         }
 
+        // If there are any new jobs being added to the processor, move them
+        // from the newJobQueue to the end of the readyQueue.
+        for (job = cpu.newJobQueue.first(); job != null; job = cpu.newJobQueue.first())
+        {
+            job.enqueueEnd(cpu.readyQueue);
+        }
+
         // If the CPU is ready for a new job, start that job
         if (cpu.currentJob.empty())
         {
-            // Find the highest priority job in the ready queue
-            best = null;
-            bestPriority = -999;
-            for (job = cpu.readyQueue.first(); job != null; job = job.next())
+            Job first_job = cpu.readyQueue.first();
+            Job highest = first_job;
+
+            for (Job j = first_job; j != null; j = j.next())
             {
-                curPriority = job.curPriority();
-                if (best == null || curPriority > bestPriority)
+                if (j.curPriority() > highest.curPriority())
                 {
-                    best = job;
-                    bestPriority = curPriority;
+                    highest = j;
                 }
             }
-            if (best != null)
+
+            if (highest != null)
             {
-                best.enqueueStart(cpu.currentJob);
-                setQuanta(best.curPriority());
+               highest.enqueueStart(cpu.currentJob);
+               setQuanta(highest.curPriority());
             }
         }
     }
 
     public void testCase1(Processor cpu)
     {
-        cpu.addJob(1, 1, 50, "10,20,30,40", 1, 128);
-        cpu.addJob(2, 1, 30, "7,15,22", 3, 103);
-        cpu.addJob(3, 1, 20, "6,14", 2, 74);
-        cpu.addJob(4, 1, 20, "6,14", 3, 85);
+        cpu.addJob(1, 1, 50, "10,20,30,40", 1, 141);
+        cpu.addJob(2, 1, 30, "7,15,22", 3, 71);
+        cpu.addJob(3, 1, 20, "6,14", 2, 50);
+        cpu.addJob(4, 1, 20, "6,14", 3, 63);
     }
 
     public void testCase2(Processor cpu)
     {
-        cpu.addJob(1, 0, 30, "9,18,27", 4, 210);
-        cpu.addJob(2, 1, 20, "3,6,9,12,15", 5, 104);
+        cpu.addJob(1, 0, 30, "9,18,27", 4, 195);
+        cpu.addJob(2, 1, 20, "3,6,9,12,15", 5, 87);
         cpu.addJob(3, 2, 30, "5,10,15,20,25", 2, 120);
-        cpu.addJob(4, 21, 21, "20", 4, 179);
-        cpu.addJob(5, 42, 29, "28", 3, 198);
-        cpu.addJob(6, 71, 18, "5,10,15", 3, 135);
-        cpu.addJob(7, 89, 13, "4,8,12", 4, 136);
-        cpu.addJob(8, 102, 19, "4,8,12,16", 2, 158);
-        cpu.addJob(9, 121, 19, "9,18", 1, 217);
-        cpu.addJob(10, 140, 26, "6,12,18,24", 1, 234);
+        cpu.addJob(4, 21, 21, "20", 4, 191);
+        cpu.addJob(5, 42, 29, "28", 3, 192);
+        cpu.addJob(6, 71, 18, "5,10,15", 3, 139);
+        cpu.addJob(7, 89, 13, "4,8,12", 4, 121);
+        cpu.addJob(8, 102, 19, "4,8,12,16", 2, 175);
+        cpu.addJob(9, 121, 19, "9,18", 1, 211);
+        cpu.addJob(10, 140, 26, "6,12,18,24", 1, 239);
     }
 }
