6df0b4a7173bdc07756f94bf54bfd51ba72ae104
Taylor Thurlow <taylorthurlow8@gmail.com>
2018-09-27 14:07:10 -0700

    CS2600: HW1, finish one-liner solution

--- cs2600/randword.bash
+++ cs2600/randword.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-DICT=$(cat /usr/share/dict/words)
-NUM_WORDS=$(cat $DICT | wc -l)
-INDEX=$RANDOM % $NUM_WORDS 
-WORD=$(cat $DICT | head -n $INDEX | tail -n 1)
-echo $WORD
+cat /usr/share/dict/words | head -n $(($(od -N 4 -t uL -An /dev/urandom | tr -d " ") % $(cat /usr/share/dict/words | wc -l))) | tail -n 1
+
+# Pipes the dictionary into head/tail to get a single entry. The entry is selected
+# using /dev/urandom because $RANDOM does not provide suitably large numbers for random
+# selection. Instead 'od' is used to get 4 bytes from the top of /dev/urandom, as an
+# unsigned long. That number is piped into 'tr' in order to remove some empty spaces
+# that come from /dev/urandom. That random number is used with the line count of the
+# dictionary to select a random number between 0 and the number of words in the
+# dictionary.
