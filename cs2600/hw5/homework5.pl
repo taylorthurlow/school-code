ff0f8bd9cae71bb404a984564b693920f601eab0
Taylor Thurlow <taylorthurlow8@gmail.com>
2018-11-29 19:43:56 -0800

    CS2600: HW5, finished

--- cs2600/hw5/homework5.pl
+++ cs2600/hw5/homework5.pl
@@ -1,5 +1,22 @@
 #!/usr/bin/env perl
 
+# Parses output of https://nocable.org/tv-listings/bm95-pomona-ca-91768
+# to show shows on channels 2.1, 4.1, and 7.1.
+
 while(<>) { push @lines, $_ }
 $data = join "", @lines;
-# @matches = $data =~
+# @matches = $data =~ /<b>2\.1<\/b>\s+<\/th>\s+<td[\S\s]+?(?:title)">([\w ]+)/;
+
+@r = matchPage("2\.1");
+print("2.1 - " . $r[0]);
+print("\n");
+@r = matchPage("4\.1");
+print("4.1 - " . $r[0]);
+print("\n");
+@r = matchPage("7\.1");
+print("7.1 - " . $r[0]);
+print("\n");
+
+sub matchPage {
+  return $data =~ /<b>$_[0]<\/b>\s+<\/th>\s+<td[\S\s]+?(?:title)">([\w ]+)/;
+}
